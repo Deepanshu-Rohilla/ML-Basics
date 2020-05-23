d73ecff558ad2f4ef091b9e375c8d6fb75787bf3
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-05-23 23:18:14 +0530

    Basics of classifier

---
+++ classifier1.py
@@ -0,0 +1,12 @@
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+
+iris = datasets.load_iris()
+# print(iris.DESCR)
+features = iris.data
+labels = iris.target
+clf = KNeighborsClassifier()
+clf.fit(features,labels)
+preds = clf.predict([[31,1,1,1]])
+print(preds)
